e5405435e93b7f556efd1f8c5ab0ef86b5ff8eaa
Nguyễn Bá Tâm <94305852+Tam1904@users.noreply.github.com>
2022-12-04 23:35:49 +0700

    Update GitTest.java

--- redis/src/main/java/com/example/redis/GitTest.java
+++ redis/src/main/java/com/example/redis/GitTest.java
@@ -3,6 +3,6 @@ package com.example.redis;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("Git nè");
+        System.out.println("Git nè haha");
     }
 }
